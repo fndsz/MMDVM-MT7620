d9625675da13c85910b3e3aedcc02fa8e68303cf
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-05 17:06:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
+git clone https://github.com/siwind/luci-app-wolplus package/wolplus
+git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/luci-app-tailscale-community
